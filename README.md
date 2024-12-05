d5600afbf1b11435bdfa1fabab23f73b5101d61e
Epistememos <signup.yy@gmail.com>
2024-12-05 08:58:14 -0500

    Fixed pathing issue - Also tried static development but simpler to just use Vercel due to dynamic path

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Welcome to my website repo.
 Built proudly using NEXT.js, Typescript & Tailwind
-Website viewable on: epistememos.github.io
+Website viewable on: youdas.yessad
 To start the website locally: npm run dev
 Lmk if there are any bugs 😉
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -30,11 +30,11 @@ export default function Navbar() {
                 <div className={`fixed top-0 left-0 w-full z-50 flex py-2 h-max justify-between text-white text-lg bg-slate-900 bg-opacity-30 transition-opacity duration-300 ${showNavbar ? "opacity-100" : "opacity-0 -translate-y-full"}`}>
                     <h1 className="text-4xl px-4 ">YOUDAS YESSAD</h1>
                     <div className="flex align-middle divide-x text-right px-2  ">
-                        <a href="" className="p-2 rounded-l-md hover:bg-slate-900 hover:bg-opacity-25">HOME</a>
-                        <a href="media" className="p-2 hover:bg-slate-900 hover:bg-opacity-25">MEDIA</a>
-                        <a href="involvements" className="p-2 hover:bg-slate-900 hover:bg-opacity-25">INVOLVEMENTS</a>
-                        <a href="projects" className="p-2 hover:bg-slate-900 hover:bg-opacity-25">PROJECTS</a>
-                        <a href="achievements" className="p-2 rounded-r-md hover:bg-slate-900 hover:bg-opacity-25">ACHIEVEMENTS</a>
+                        <a href="/" className="p-2 rounded-l-md hover:bg-slate-900 hover:bg-opacity-25">HOME</a>
+                        <a href="/media" className="p-2 hover:bg-slate-900 hover:bg-opacity-25">MEDIA</a>
+                        <a href="/involvements" className="p-2 hover:bg-slate-900 hover:bg-opacity-25">INVOLVEMENTS</a>
+                        <a href="/projects" className="p-2 hover:bg-slate-900 hover:bg-opacity-25">PROJECTS</a>
+                        <a href="/achievements" className="p-2 rounded-r-md hover:bg-slate-900 hover:bg-opacity-25">ACHIEVEMENTS</a>
                     </div>
                 </div>
             </div>
